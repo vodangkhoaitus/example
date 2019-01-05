40cabf98ba56fa23d1d0daa567d3105a5bd89c0c
1512263 <1512263@student.hcmus.edu.vn>
2019-01-05 13:06:44 +0700

    --commit

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
-var mongo = require('mongodb').MongoClient;
-var url = "mongodb://1512263:munimuni@examplecluster-shard-00-00-zn1em.mongodb.net:27017,examplecluster-shard-00-01-zn1em.mongodb.net:27017,examplecluster-shard-00-02-zn1em.mongodb.net:27017/test?ssl=true&replicaSet=ExampleCluster-shard-0&authSource=admin&retryWrites=true"
 
-mongo.connect(url,, function(err, db) {
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb+srv://1512263:munimuni@examplecluster-zn1em.mongodb.net/test?retryWrites=true"
+
+mongo.connect(url, {useNewUrlParser:true},function(err, db) {
     if (err) throw err;
     var dbo = db.db("ExampleDB");
     dbo.collection("Books").find({}).toArray(function(err, result) {
       if (err) throw err;
-      console.log(result.name);
+      console.log(result);
       db.close();
     });
   });
