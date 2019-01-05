04a29a6114d7bea0e8bc51099547ba241b3b3060
1512263 <1512263@student.hcmus.edu.vn>
2019-01-05 13:31:41 +0700

    --commit

--- database.js
+++ database.js
@@ -2,12 +2,12 @@
 var mongo = require('mongodb').MongoClient;
 var url = "mongodb+srv://1512263:munimuni@examplecluster-zn1em.mongodb.net/test?retryWrites=true"
 
-mongo.connect(url, {useNewUrlParser:true},function(err, db) {
+mongo.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("ExampleDB");
     dbo.collection("Books").find({}).toArray(function(err, result) {
       if (err) throw err;
-      console.log(result);
+      console.log(result.name);
       db.close();
     });
   });
